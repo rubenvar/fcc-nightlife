4e0809e417279fda6b03303f09d71965684891d7
Rubén <hey@rubenvara.io>
2018-05-07 13:11:18 +0300

    yelp api request basics

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,11 +1,24 @@
+
+const axios = require('axios');
+
 exports.renderHome = (req, res) => {
   res.render('homepage');
 }
 
 exports.searchResults = (req, res) => {
   // gets the searched word
-  res.json(req.body.location);
-  // needs to connect to api and get results 
-  // and maybe axios them in a div below the homepage form and make the form dissapear
+  const location = req.body.location;
+  const uri = 'https://api.yelp.com/v3/businesses/search?limit=10&categories=bars&location=' + location;
+  const token = process.env.YELP_KEY;
+  axios
+    // connects to api
+    .get(uri, { headers: { "Authorization": `Bearer ${token}` } })
+    // and maybe axios them in a div below the homepage form and make the form dissapear
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   // and add a button 'new search' that hides the div and shows the form again
 }
--- public/javascripts/nightlife-app.js
+++ public/javascripts/nightlife-app.js
@@ -1,3 +1,7 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import displayResults from './modules/search'
+
+const searchForm = $('form.search-location');
+searchForm.on('submit', displayResults);
